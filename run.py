b1f01627f3010e8d5f217ffbcde6d51b933d930d
unknown <ayush.ibotix@gmail.com>
2025-07-22 15:04:35 +0530

    changed in run py file

--- run.py
+++ run.py
@@ -1,5 +1,11 @@
 # run.py
-import uvicorn
+# import uvicorn
 
+# if __name__ == "__main__":
+#     uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+import os
+import uvicorn
+ 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))  # Render sets this env var
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
